c51ffb0b359940fe09e704675d0297c4e3574dd8
Damian Dąbrowski <dabek1812@gmail.com>
2021-11-11 21:22:28 +0000

    Add script

---
+++ down-cm.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+in1 = 18
+in2 = 16
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+
+GPIO.output(in1, False)
+GPIO.output(in2, False)
+
+height = input("How many cm: ")
+
+GPIO.output(in1, True)
+
+time.sleep(int(height)/2.5)
+
+GPIO.output(in1, False)
+
+GPIO.cleanup()
---
+++ up.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+in1 = 18
+in2 = 16
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+
+GPIO.output(in1, False)
+GPIO.output(in2, False)
+
+try:
+    while True:
+        GPIO.output(in2, True)
+
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
