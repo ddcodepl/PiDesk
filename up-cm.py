10a8acc84379071d9734a59e232e9982c00016c2
Damian Dąbrowski <dabek1812@gmail.com>
2021-12-26 19:00:08 +0100

    Update data file

--- up-cm.py
+++ up-cm.py
@@ -5,6 +5,11 @@ import sys
 in1 = 18
 in2 = 16
 
+with open("/home/ubuntu/desk/height.txt", "r") as file:
+    current_height = file.readline()
+    for current_height in file:
+        pass
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
@@ -20,6 +25,10 @@ else:
 GPIO.output(in2, True)
 
 time.sleep(int(height)/2.5)
+
+f = open("/home/ubuntu/desk/height.txt", "w")
+f.write(str(int(current_height) + int(height)))
+f.close()
 
 GPIO.output(in2, False)
 
